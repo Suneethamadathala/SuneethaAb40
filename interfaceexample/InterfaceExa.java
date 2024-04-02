09c5c8d27ed05228e2e59a2d6fea5c2ad3e91872
Suneetha madathala4 <suneethamadathala23@gmail.com>
2024-04-02 18:55:36 +0100

    empty

--- interfaceexample/InterfaceExa.java
+++
@@ -1,5 +0,0 @@
-package interfaceexample;
-
-public class InterfaceExa {
-
-}
--- myjavapackage/MyDemoProgram.java
+++
@@ -1,9 +0,0 @@
-package myjavapackage;
-
-public class MyDemoProgram {
-	public static void main(String[] args)
-	{
-		System.out.println("Suneetha");
-	}
-
-}
